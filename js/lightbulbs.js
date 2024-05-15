cc0d3bc813309a47397162b73a4f8672830e982f
MauroMakowka <mauuro@op.pl>
2024-05-15 09:29:47 +0200

    poprawa karty do slidera

--- js/lightbulbs.js
+++ js/lightbulbs.js
@@ -1,80 +1,99 @@
+let plus = document.querySelector('.plus'),
+	minus = document.querySelector('.minus'),
+	num = document.querySelector('.num')
+let a = 1
+plus.addEventListener('click', () => {
+	a++
+	a = a < 10 ? '0' + a : a
+	num.innerText = a
+	console.log(a)
+})
+
+minus.addEventListener('click', () => {
+	if (a > 1) {
+		a--
+		a = a < 10 ? '0' + a : a
+		num.innerText = a
+	}
+})
+
 var container = document.getElementById('container')
-var slider = document.getElementById('slider');
-var slides = document.getElementsByClassName('slide').length;
-var buttons = document.getElementsByClassName('btn');
+var slider = document.getElementById('slider')
+var slides = document.getElementsByClassName('slide').length
+var buttons = document.getElementsByClassName('btn')
 
-var currentPosition = 0;
-var currentMargin = 0;
-var slidesPerPage = 0;
-var slidesCount = slides - slidesPerPage;
-var containerWidth = container.offsetWidth;
-var prevKeyActive = false;
-var nextKeyActive = true;
+var currentPosition = 0
+var currentMargin = 0
+var slidesPerPage = 0
+var slidesCount = slides - slidesPerPage
+var containerWidth = container.offsetWidth
+var prevKeyActive = false
+var nextKeyActive = true
 
-window.addEventListener("resize", checkWidth);
+window.addEventListener('resize', checkWidth)
 
 function checkWidth() {
-	containerWidth = container.offsetWidth;
-	setParams(containerWidth);
+	containerWidth = container.offsetWidth
+	setParams(containerWidth)
 }
 
 function setParams(w) {
 	if (w < 551) {
-		slidesPerPage = 1;
+		slidesPerPage = 1
 	} else {
 		if (w < 901) {
-			slidesPerPage = 2;
+			slidesPerPage = 2
 		} else {
 			if (w < 1101) {
-				slidesPerPage = 3;
+				slidesPerPage = 3
 			} else {
-				slidesPerPage = 4;
+				slidesPerPage = 5
 			}
 		}
 	}
-	slidesCount = slides - slidesPerPage;
+	slidesCount = slides - slidesPerPage
 	if (currentPosition > slidesCount) {
-		currentPosition -= slidesPerPage;
-	};
-	currentMargin = - currentPosition * (100 / slidesPerPage);
-	slider.style.marginLeft = currentMargin + '%';
+		currentPosition -= slidesPerPage
+	}
+	currentMargin = -currentPosition * (100 / slidesPerPage)
+	slider.style.marginLeft = currentMargin + '%'
 	if (currentPosition > 0) {
-		buttons[0].classList.remove('inactive');
+		buttons[0].classList.remove('inactive')
 	}
 	if (currentPosition < slidesCount) {
-		buttons[1].classList.remove('inactive');
+		buttons[1].classList.remove('inactive')
 	}
 	if (currentPosition >= slidesCount) {
-		buttons[1].classList.add('inactive');
+		buttons[1].classList.add('inactive')
 	}
 }
 
-setParams();
+setParams()
 
 function slideRight() {
 	if (currentPosition != 0) {
-		slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
-		currentMargin += (100 / slidesPerPage);
-		currentPosition--;
-	};
+		slider.style.marginLeft = currentMargin + 100 / slidesPerPage + '%'
+		currentMargin += 100 / slidesPerPage
+		currentPosition--
+	}
 	if (currentPosition === 0) {
-		buttons[0].classList.add('inactive');
+		buttons[0].classList.add('inactive')
 	}
 	if (currentPosition < slidesCount) {
-		buttons[1].classList.remove('inactive');
+		buttons[1].classList.remove('inactive')
 	}
-};
+}
 
 function slideLeft() {
 	if (currentPosition != slidesCount) {
-		slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
-		currentMargin -= (100 / slidesPerPage);
-		currentPosition++;
-	};
+		slider.style.marginLeft = currentMargin - 100 / slidesPerPage + '%'
+		currentMargin -= 100 / slidesPerPage
+		currentPosition++
+	}
 	if (currentPosition == slidesCount) {
-		buttons[1].classList.add('inactive');
+		buttons[1].classList.add('inactive')
 	}
 	if (currentPosition > 0) {
-		buttons[0].classList.remove('inactive');
+		buttons[0].classList.remove('inactive')
 	}
-};
+}
