aa918e69d15f83bd05ae10b846abd0ff75e9c19f
Pekowy <126596895+Pekowy@users.noreply.github.com>
2024-05-16 11:20:43 +0200

    #2 90% Skończony

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -4,5 +4,12 @@ const menuAside = document.querySelector('.burger__menu--aside')
 
 toggleBtn.onclick = function(){
     menuAside.classList.toggle('open')
+    toggleBtn.classList.toggle('open')
+    const isOpen = toggleBtn.classList.contains('open')
 
-}
+    if (isOpen) {
+    document.querySelector('.open').innerHTML = '<img class="toggle-btn--icon" src="./images/xmark-solid.svg" alt="🍔">';
+    } else {
+    document.querySelector('.burger__menu--toggle-btn').innerHTML = '<img class="toggle-btn--icon" src="./images/burger.png" alt="🍔">';
+    }
+}   
