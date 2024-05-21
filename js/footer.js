150023308022a1b28c46863ef974f7b36973f584
Pszczola2115 <mikolaj.pszczola@wp.pl>
2024-05-21 12:59:15 +0200

    #7 zmiany w js

--- js/footer.js
+++ js/footer.js
@@ -1,75 +1,31 @@
-function toggleElementVisibility1() {
-    let x = document.getElementById("footer__mobileView1");
-    if (x.style.display === "block") {
-        document.getElementById("changeIcon1").classList.add('footer-top');
-        document.getElementById("changeIcon1").classList.remove('footer-bottom');
-        if ( document.getElementById("changeIcon1").classList.contains('footer-bottom') )
-        document.getElementById("changeIcon1").classList.toggle('footer-top');
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButtons = document.querySelectorAll('.footer__mobileViewBtn');
 
-        x.style.display = "none";
-    } else {
-        document.getElementById("changeIcon1").classList.add('footer-bottom');
-        document.getElementById("changeIcon1").classList.remove('footer-top');
-        if ( document.getElementById("changeIcon1").classList.contains('footer-top') )
-        document.getElementById("changeIcon1").classList.toggle('footer-bottom');
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const icon = event.currentTarget.querySelector('i');
+            const targetId = icon.getAttribute('data-toggle');
+            const targetElement = document.querySelector(`#${targetId}`);
 
-        x.style.display = "block";
-    }
-}
+            if (targetElement.style.display === "block") {
+                icon.classList.add('footer-top');
+                icon.classList.remove('footer-bottom');
+                targetElement.style.display = "none";
+            } else {
+                icon.classList.add('footer-bottom');
+                icon.classList.remove('footer-top');
+                targetElement.style.display = "block";
+            }
+        });
+    });
+});
 
-function toggleElementVisibility2() {
-    let x = document.getElementById("footer__mobileView2");
-    if (x.style.display === "block") {
-        document.getElementById("changeIcon2").classList.add('footer-top');
-        document.getElementById("changeIcon2").classList.remove('footer-bottom');
-        if ( document.getElementById("changeIcon2").classList.contains('footer-bottom') )
-        document.getElementById("changeIcon2").classList.toggle('footer-top');
 
-        x.style.display = "none";
-    } else {
-        document.getElementById("changeIcon2").classList.add('footer-bottom');
-        document.getElementById("changeIcon2").classList.remove('footer-top');
-        if ( document.getElementById("changeIcon2").classList.contains('footer-top') )
-        document.getElementById("changeIcon2").classList.toggle('footer-bottom');
 
-        x.style.display = "block";
-    }
-}
 
-function toggleElementVisibility3() {
-    let x = document.getElementById("footer__mobileView3");
-    if (x.style.display === "block") {
-        document.getElementById("changeIcon3").classList.add('footer-top');
-        document.getElementById("changeIcon3").classList.remove('footer-bottom');
-        if ( document.getElementById("changeIcon3").classList.contains('footer-bottom') )
-        document.getElementById("changeIcon3").classList.toggle('footer-top');
 
-        x.style.display = "none";
-    } else {
-        document.getElementById("changeIcon3").classList.add('footer-bottom');
-        document.getElementById("changeIcon3").classList.remove('footer-top');
-        if ( document.getElementById("changeIcon3").classList.contains('footer-top') )
-        document.getElementById("changeIcon3").classList.toggle('footer-bottom');
 
-        x.style.display = "block";
-    }
-}
 
-function toggleElementVisibility4() {
-    let x = document.getElementById("footer__mobileView4");
-    if (x.style.display === "block") {
-        document.getElementById("changeIcon4").classList.add('footer-top');
-        document.getElementById("changeIcon4").classList.remove('footer-bottom');
-        if ( document.getElementById("changeIcon4").classList.contains('footer-bottom') )
-        document.getElementById("changeIcon4").classList.toggle('footer-top');
 
-        x.style.display = "none";
-    } else {
-        document.getElementById("changeIcon4").classList.add('footer-bottom');
-        document.getElementById("changeIcon4").classList.remove('footer-top');
-        if ( document.getElementById("changeIcon4").classList.contains('footer-top') )
-        document.getElementById("changeIcon4").classList.toggle('footer-bottom');
 
-        x.style.display = "block";
-    }
-}
+
